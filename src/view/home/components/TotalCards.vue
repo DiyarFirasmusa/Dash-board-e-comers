61ca0290362d7972b5dd92e85e8df43d68757abb
DiyarFirasmusa <diyarfirasoffical@gmail.com>
2025-03-18 09:31:55 +0300

    d
    d

--- src/view/home/components/TotalCards.vue
+++ src/view/home/components/TotalCards.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-import Card from './Card.vue';
+import CardUsers from './CardUsers.vue';
+import CardOrder from './CardOrder.vue';
+import CardSalers from './CardSalers.vue';
+import CardPending  from './CardPending.vue'
+
 </script>
 <template>
     <div class="w-full h-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 ">
       <div class="w-full">
-        <Card />
+        <CardUsers />
       </div>
       <div class="w-full">
-        <Card />
+        <CardOrder />
       </div>
       <div class="w-full">
-        <Card />
+        <CardSalers />
       </div>
       <div class="w-full">
-        <Card />
+        <CardPending />
       </div>
     </div>
   </template>
